6735477eed0a81ef9ae09d6cd5d057d2334732ce
wroten <xionghu@sunline.cn>
2021-09-07 00:58:37 +0800

    项目初始结构创建20210906

--- mall-product/src/test/java/com/wroten/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/wroten/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wroten.mall.product;
 
+import com.wroten.mall.product.entity.BrandEntity;
+import com.wroten.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+
+        brandService.save(brandEntity);
+        System.out.println("保存成功！");
     }
 
 }
