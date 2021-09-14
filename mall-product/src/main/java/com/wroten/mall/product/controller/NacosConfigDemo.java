67ed4aa92858b7d2ae2023882312ab8cce82b25c
wroten <xionghu@sunline.cn>
2021-09-14 19:57:03 +0800

    项目初始结构创建20210914

--- mall-product/src/main/java/com/wroten/mall/product/controller/NacosConfigDemo.java
+++ mall-product/src/main/java/com/wroten/mall/product/controller/NacosConfigDemo.java
@@ -1,20 +1,19 @@
 package com.wroten.mall.product.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
-@RefreshScope
+@RefreshScope //在控制器类加入@RefreshScope注解使当前类下的配置支持Nacos的动态刷新功能。
 public class NacosConfigDemo {
 
-    @Resource
+    @Value("${config.info}")
     private String configInfo;
 
-    @RequestMapping("/config/info")
-    public String getConfigInfo(){
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
         return configInfo;
     }
 }
