a40e81544900ca236786e156de0a01534c022b27
wroten <xionghu@sunline.cn>
2021-09-19 00:17:29 +0800

    项目初始结构创建20210918

--- mall-product/src/test/java/com/wroten/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/wroten/mall/product/MallProductApplicationTests.java
@@ -2,11 +2,11 @@ package com.wroten.mall.product;
 
 import com.wroten.mall.product.entity.BrandEntity;
 import com.wroten.mall.product.service.BrandService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MallProductApplication.class)
 class MallProductApplicationTests {
     @Autowired
     BrandService brandService;
