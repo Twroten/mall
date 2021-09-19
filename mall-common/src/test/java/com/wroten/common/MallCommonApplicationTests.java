3d5b54609813d8dcae6a7c4c2e68838bd38122ea
wroten <xionghu@sunline.cn>
2021-09-20 04:04:04 +0800

    项目初始结构创建20210919

--- mall-common/src/test/java/com/wroten/common/MallCommonApplicationTests.java
+++ mall-common/src/test/java/com/wroten/common/MallCommonApplicationTests.java
@@ -1,13 +1,20 @@
 package com.wroten.common;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MallCommonApplicationTests {
 
     @Test
     void contextLoads() {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //密钥salt
+        textEncryptor.setPassword("MTEyNQ==");
+        //要加密的数据(数据库的用户名或密码)
+        String passwordCipher = textEncryptor.encrypt("root");
+        System.out.println("passwordCipher = " + passwordCipher);
     }
 
 }
